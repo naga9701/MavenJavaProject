ead2e15bf4ced531ac47b1f2971291d3b86a6bbf
nagarjuna <nagarjuna@gmail.com>
2022-10-12 13:16:08 +0530

    google cmmit

---
+++ JavaProject1forPrograms/src/programs/StringProgram.java
@@ -0,0 +1,19 @@
+package programs;
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		
+		
+		String str  = "hello world";
+		
+		String str1= str.toUpperCase();
+		
+		System.out.println(str1);
+		
+		
+		
+	}
+	
+	
+}
--- JavaProject1forPrograms/src/programs/test3.java
+++ JavaProject1forPrograms/src/programs/test3.java
@@ -1,13 +1,26 @@
 package programs;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 class test3{
 
 public static void main(String []args){
 
 
+	System.setProperty("webdriver.gecko.driver", "C:\\Users\\MY PC\\Desktop\\Testing prep\\geckodriver-v0.31.0-win64\\geckodriver.exe");
+	
+	WebDriver driver = new FirefoxDriver();
+	driver.get("https://www.google.com");
+	
+	
+	driver.findElement(By.cssSelector("input[class = 'gLFyf gsfi']")).sendKeys("www.amazon.com", Keys.ENTER);
+	
+//driver.findElement(By.cssSelector("input[class='gNO89b']")).click();
 
-
-
+System.out.println(driver.getTitle());
 
 
 }
