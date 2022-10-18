eb88ffaaaa4727713fb03309e2ba910eb3600ca3
nagarjuna <nagarjuna@gmail.com>
2022-10-18 22:47:52 +0530

    commit1

--- JavaProject1forPrograms/src/programs/StringReverse.java
+++ JavaProject1forPrograms/src/programs/StringReverse.java
@@ -1,5 +1,30 @@
 package programs;
 
+import java.util.Scanner;
+
 public class StringReverse {
 
+	public StringReverse(String str) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		str = sc.nextLine();
+String reverse = "";
+		for(int i = str.length()-1;i>=0;i--) {
+			
+			reverse = str.charAt(i)+
+			
+		}
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
 }
